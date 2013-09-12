cb3179df0e7303f802d4f7c694bd97f3ebf9caea
CalTrainsd <caltrainsd@gmail.com>
2013-09-12 12:05:57 -0700

    blank+plugins

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+C:\markf\pg3.0\baz\platforms\android\gen\io\cordova\hellocordova\R.java \
+ : C:\markf\pg3.0\baz\platforms\android\res\drawable\icon.png \
+C:\markf\pg3.0\baz\platforms\android\res\drawable-hdpi\icon.png \
+C:\markf\pg3.0\baz\platforms\android\res\drawable-hdpi\ic_launcher.png \
+C:\markf\pg3.0\baz\platforms\android\res\drawable-ldpi\icon.png \
+C:\markf\pg3.0\baz\platforms\android\res\drawable-ldpi\ic_launcher.png \
+C:\markf\pg3.0\baz\platforms\android\res\drawable-mdpi\icon.png \
+C:\markf\pg3.0\baz\platforms\android\res\drawable-mdpi\ic_launcher.png \
+C:\markf\pg3.0\baz\platforms\android\res\drawable-xhdpi\icon.png \
+C:\markf\pg3.0\baz\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+C:\markf\pg3.0\baz\platforms\android\res\layout\main.xml \
+C:\markf\pg3.0\baz\platforms\android\res\values\strings.xml \
+C:\markf\pg3.0\baz\platforms\android\res\xml\config.xml \
+C:\markf\pg3.0\baz\platforms\android\bin\AndroidManifest.xml \
